391b12df13e2920e49508210dd9ad1ee2ae0ca61
Peerasit <98304476+PeerasitPloyaram@users.noreply.github.com>
2023-03-08 06:25:10 +0000

    test

--- test/test.cpp
+++ test/test.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <chrono>
-
 using namespace std;
 
-vector <int> cul(int pos, int temp){
-    vector <int> data;
-    for (int i = 0; i < pos; i++){
-       temp += i; 
-       data.push_back(temp);
-    }
-    return data;
-}
-
-int main() {
-    vector <int> data = cul(10,3);
-    for (int i = data.size() ;i > 0; i--){
-        cout << i;
-        cout << '\n';
-    }
-    cout << '\n';
-    return 0;
+int main(){
+    cout << "Hello world!";
 }
